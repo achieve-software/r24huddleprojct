cdcb8ccc93f116673fdbd014bf2a88bb4c01aaaf
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-19 16:27:26 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GlobalStyles from "./components/styles/Global.styled";
 import { Container } from "./components/styles/Container.styled";
 import Card from "./components/Card";
 import content from './content'
+import Footer from "./components/Footer";
 function App() {
   const theme = {
     colors: {
@@ -20,6 +21,7 @@ function App() {
         <Card key={index} item={item}/>
        ))}
       </Container>
+      <Footer/>
     </ThemeProvider>
   );
 }export default App;
