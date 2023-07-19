4ed645d516e932cd6cc808c99fa0a01d596b719f
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-19 14:21:24 +0300

    started

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Logo, StyledHeader } from "./styles/Header.styled";
+import { Button } from './styles/Button.styled'
+import { Container } from "./styles/Container.styled";
+
+export default function Header() {
+    return (   
+            <StyledHeader>
+       <h1>Build The Community Your Fans Will Love</h1>
+       </StyledHeader>  
+       
+       )
+}
